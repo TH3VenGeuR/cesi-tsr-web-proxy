757278f6ffda3a426eb3ccb80f9c607de3406bf0
TH3VenGeuR <88973190+TH3VenGeuR@users.noreply.github.com>
2026-01-27 14:34:25 +0100

    Create ip-service.py

---
+++ ip-service.py
@@ -0,0 +1,28 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import socket
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        hostname = socket.gethostname()
+        ip = socket.gethostbyname(hostname)
+
+        html = f"""
+        <html>
+            <head><title>Backend Python</title></head>
+            <body>
+                <h1>Backend Python</h1>
+                <p>Hostname : {hostname}</p>
+                <p>IP : {ip}</p>
+            </body>
+        </html>
+        """
+
+        self.send_response(200)
+        self.send_header("Content-Type", "text/html")
+        self.end_headers()
+        self.wfile.write(html.encode("utf-8"))
+
+if __name__ == "__main__":
+    server = HTTPServer(("0.0.0.0", 8080), Handler)
+    print("Server running on port 8080")
+    server.serve_forever()
